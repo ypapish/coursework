fe084ca0b7da1d72e25504b445acf340524c8959
Yana <155889900+ypapish@users.noreply.github.com>
2024-05-27 23:59:04 +0300

    removed the antipattern

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } 
-    else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } 
-    else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } 
-    else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } 
-    else {
-      display.innerText += item.id;
-    }
-  };
-});
+for (const button of buttons) {
+    button.onclick = () => {
+      if (button.id == "clear") {
+        display.innerText = "";
+      } 
+      else if (button.id == "backspace") {
+        let string = display.innerText.toString();
+        display.innerText = string.substr(0, string.length - 1);
+      } 
+      else if (display.innerText != "" && button.id == "equal") {
+        display.innerText = eval(display.innerText);
+      } 
+      else if (display.innerText == "" && button.id == "equal") {
+        display.innerText = "Empty!";
+        setTimeout(() => (display.innerText = ""), 2000);
+      } 
+      else {
+        display.innerText += button.id;
+      }
+    };
+  }
+  
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
